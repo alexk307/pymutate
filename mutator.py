f0a4fedc9b3b129f8e0f2b3e507be6a440681d1d
Alex Kahan <alex@zerofox.com>
2016-10-18 22:01:51 -0400

    First commit with working code

---
+++ mutator.py
@@ -0,0 +1,30 @@
+import ast
+import codegen
+from types import MethodType
+
+
+class Mutator(object):
+
+    def __init__(self):
+        self.opposite_tokens = {ast.Eq: ast.NotEq, ast.In: ast.NotIn}
+        self.transformers = []
+
+    def generate_transformers(self):
+        for token in self.opposite_tokens:
+            transformer = type('%sTransformer' % token.__name__, (ast.NodeTransformer,), {})
+            opposite = self.opposite_tokens[token]
+
+            def closure(opposite):
+                def wrap(self, node):
+                    return opposite()
+                return wrap
+            func_name = 'visit_%s' % token.__name__
+            closure.__name__ = func_name
+            setattr(transformer, func_name, MethodType(closure(opposite), None, transformer))
+            self.transformers.append(transformer)
+
+    def apply_transformations(self, code):
+        tree = ast.parse(code)
+        for transformation in self.transformers:
+            transformation().visit(tree)
+        return codegen.to_source(tree)
