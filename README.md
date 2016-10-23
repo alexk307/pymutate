9793968be6d5847eba1f6e94ca7873ce90ebab43
Alex Kahan <alex@zerofox.com>
2016-10-23 10:04:50 -0400

    Documenting

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # pymutate
 Mutation Testing Using The AST
+
+```
+import ast
+from mutator import Mutator
+
+# Define tokens to mutate
+mutations = {ast.Eq: ast.NotEq}
+mut = Mutator(mutations)
+mut.generate_transformers()
+
+# Read code to mutate
+with file('code.py', 'r') as f:
+    code = f.read()
+
+mutated_code = mut.apply_transformers()
+```
--- mutator.py
+++ mutator.py
@@ -4,18 +4,25 @@ from types import MethodType
 
 
 class Mutator(object):
+    """
+    Class to mutate Python code
+    """
 
-    def __init__(self):
-        self.opposite_tokens = {ast.Eq: ast.NotEq, ast.In: ast.NotIn}
+    def __init__(self, mutations):
+        self.opposite_tokens = mutations
         self.transformers = []
 
     def generate_transformers(self):
+        """
+        Generates the transformers to mutate the code
+        """
         for token in self.opposite_tokens:
             transformer = type('%sTransformer' % token.__name__, (ast.NodeTransformer,), {})
             opposite = self.opposite_tokens[token]
 
             def closure(opposite):
                 def wrap(self, node):
+                    self.generic_visit(node)
                     return opposite()
                 return wrap
             func_name = 'visit_%s' % token.__name__
@@ -24,6 +31,11 @@ class Mutator(object):
             self.transformers.append(transformer)
 
     def apply_transformations(self, code):
+        """
+        Applies the created transformers to the given code segment
+        :param code: Python code to transform
+        :return: Transformed Python code with mutations applied
+        """
         tree = ast.parse(code)
         for transformation in self.transformers:
             transformation().visit(tree)
